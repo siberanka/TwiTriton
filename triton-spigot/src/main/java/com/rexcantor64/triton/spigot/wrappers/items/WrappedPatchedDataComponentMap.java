91ce6a3f5273158b7fcc37067717c1932e2dc071
Diogo Correia <me@diogotc.com>
2024-06-19 00:43:04 +0100

    fix(spigot): WrappedRegistrable to DataComponentType throwing on MC 1.21
    Workaround for https://github.com/dmulloy2/ProtocolLib/issues/3027
    
    Port of 11e6d8b231834a99776df42c8a5d924cf35c2fdd to v4

--- triton-spigot/src/main/java/com/rexcantor64/triton/spigot/wrappers/items/WrappedPatchedDataComponentMap.java
+++ triton-spigot/src/main/java/com/rexcantor64/triton/spigot/wrappers/items/WrappedPatchedDataComponentMap.java
@@ -1,19 +1,26 @@
 package com.rexcantor64.triton.spigot.wrappers.items;
 
 import com.comphenix.protocol.reflect.FuzzyReflection;
+import com.comphenix.protocol.reflect.StructureModifier;
 import com.comphenix.protocol.reflect.accessors.Accessors;
+import com.comphenix.protocol.reflect.accessors.ConstructorAccessor;
 import com.comphenix.protocol.reflect.accessors.FieldAccessor;
 import com.comphenix.protocol.reflect.accessors.MethodAccessor;
 import com.comphenix.protocol.utility.MinecraftReflection;
+import com.comphenix.protocol.utility.MinecraftVersion;
 import com.comphenix.protocol.wrappers.AbstractWrapper;
 import com.comphenix.protocol.wrappers.BukkitConverters;
+import com.comphenix.protocol.wrappers.MinecraftKey;
 import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import com.comphenix.protocol.wrappers.WrappedRegistrable;
+import com.comphenix.protocol.wrappers.WrappedRegistry;
+import lombok.val;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 /**
@@ -40,10 +47,49 @@ public class WrappedPatchedDataComponentMap extends AbstractWrapper {
                     .getMethodByReturnTypeAndParameters("set", Object.class, DATA_COMPONENT_TYPE, Object.class)
     );
 
-    private static final WrappedRegistrable TYPE_CUSTOM_NAME = WrappedRegistrable.fromClassAndKey(DATA_COMPONENT_TYPE, "custom_name");
-    private static final WrappedRegistrable TYPE_LORE = WrappedRegistrable.fromClassAndKey(DATA_COMPONENT_TYPE, "lore");
-    private static final WrappedRegistrable TYPE_CONTAINER = WrappedRegistrable.fromClassAndKey(DATA_COMPONENT_TYPE, "container");
-    private static final WrappedRegistrable TYPE_WRITTEN_BOOK_CONTENT = WrappedRegistrable.fromClassAndKey(DATA_COMPONENT_TYPE, "written_book_content");
+    private static final WrappedRegistrable TYPE_CUSTOM_NAME;
+    private static final WrappedRegistrable TYPE_LORE;
+    private static final WrappedRegistrable TYPE_CONTAINER;
+    private static final WrappedRegistrable TYPE_WRITTEN_BOOK_CONTENT;
+
+    static {
+        // Workaround for https://github.com/dmulloy2/ProtocolLib/issues/3027
+        // In 1.21, there are two registries of type DataComponentType, so WrappedRegistry returns the incorrect one.
+        if (MinecraftVersion.v1_21_0.atOrAbove()) {
+            MethodAccessor registryResourceKeyMethod = Accessors.getMethodAccessor(
+                    FuzzyReflection.fromClass(MinecraftReflection.getIRegistry())
+                            .getMethodByReturnTypeAndParameters("key", MinecraftReflection.getResourceKey())
+            );
+            Object registry = Arrays.stream(Accessors.getFieldAccessorArray(MinecraftReflection.getBuiltInRegistries(), MinecraftReflection.getIRegistry(), false))
+                    .map(registryField -> registryField.get(null))
+                    .filter(registryObj -> {
+                        val resourceKey = registryResourceKeyMethod.invoke(registryObj);
+                        val modifier = new StructureModifier<MinecraftKey>(resourceKey.getClass())
+                                .withTarget(resourceKey)
+                                .withType(MinecraftReflection.getMinecraftKeyClass(), MinecraftKey.getConverter());
+                        MinecraftKey registryKey = modifier.read(0);
+                        MinecraftKey locationKey = modifier.read(1);
+                        return registryKey.getFullKey().equals("minecraft:root") && locationKey.getFullKey().equals("minecraft:data_component_type");
+                    })
+                    .findAny()
+                    .orElseThrow(() -> new RuntimeException("cannot find data component type registry for item translation"));
+
+            // Ironically, use ProtocolLib on ProtocolLib
+            ConstructorAccessor wrappedRegistryConstructor = Accessors.getConstructorAccessor(WrappedRegistry.class, Object.class);
+            WrappedRegistry wrappedRegistry = (WrappedRegistry) wrappedRegistryConstructor.invoke(registry);
+
+            // The WrappedRegistrable will have an incorrect factory, but that's fine because we only use the handle anyway
+            TYPE_CUSTOM_NAME = WrappedRegistrable.fromHandle(DATA_COMPONENT_TYPE, wrappedRegistry.get("custom_name"));
+            TYPE_LORE = WrappedRegistrable.fromHandle(DATA_COMPONENT_TYPE, wrappedRegistry.get("lore"));
+            TYPE_CONTAINER = WrappedRegistrable.fromHandle(DATA_COMPONENT_TYPE, wrappedRegistry.get("container"));
+            TYPE_WRITTEN_BOOK_CONTENT = WrappedRegistrable.fromHandle(DATA_COMPONENT_TYPE, wrappedRegistry.get("written_book_content"));
+        } else {
+            TYPE_CUSTOM_NAME = WrappedRegistrable.fromClassAndKey(DATA_COMPONENT_TYPE, "custom_name");
+            TYPE_LORE = WrappedRegistrable.fromClassAndKey(DATA_COMPONENT_TYPE, "lore");
+            TYPE_CONTAINER = WrappedRegistrable.fromClassAndKey(DATA_COMPONENT_TYPE, "container");
+            TYPE_WRITTEN_BOOK_CONTENT = WrappedRegistrable.fromClassAndKey(DATA_COMPONENT_TYPE, "written_book_content");
+        }
+    }
 
     private WrappedPatchedDataComponentMap(Object handle) {
         super(getWrappedClass());
