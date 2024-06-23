sheduxdev/Triton

fix(spigot): item translation in MC 1.20.6

core/src/main/java/com/rexcantor64/triton/utils/ItemStackTranslationUtils.java
@@ -5,15 +5,16 @@ import com.comphenix.protocol.reflect.accessors.Accessors;
 import com.comphenix.protocol.reflect.accessors.MethodAccessor;
 import com.comphenix.protocol.utility.MinecraftReflection;
 import com.comphenix.protocol.utility.MinecraftVersion;
+import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import com.comphenix.protocol.wrappers.nbt.NbtBase;
 import com.comphenix.protocol.wrappers.nbt.NbtCompound;
 import com.comphenix.protocol.wrappers.nbt.NbtFactory;
 import com.comphenix.protocol.wrappers.nbt.NbtList;
 import com.google.gson.JsonParseException;
-import com.google.gson.JsonSyntaxException;
 import com.rexcantor64.triton.Triton;
 import com.rexcantor64.triton.api.language.Localized;
 import com.rexcantor64.triton.config.MainConfig;
+import com.rexcantor64.triton.wrappers.items.*;
 import lombok.val;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -21,11 +22,11 @@ import net.md_5.bungee.chat.ComponentSerializer;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ItemStackTranslationUtils {
@@ -33,7 +34,7 @@ public class ItemStackTranslationUtils {
     private static final MethodAccessor NBT_DESERIALIZER_METHOD;
 
     static {
-        val mojangsonParserClass = MinecraftReflection.getMinecraftClass("nbt.MojangsonParser", "MojangsonParser");
+        val mojangsonParserClass = MinecraftReflection.getMinecraftClass("nbt.TagParser", "nbt.MojangsonParser", "MojangsonParser");
         FuzzyReflection fuzzy = FuzzyReflection.fromClass(mojangsonParserClass);
         val method = fuzzy.getMethodByReturnTypeAndParameters("deserializeNbtCompound", MinecraftReflection.getNBTCompoundClass(), String.class);
         NBT_DESERIALIZER_METHOD = Accessors.getMethodAccessor(method);
@@ -52,10 +53,19 @@ public class ItemStackTranslationUtils {
      * @param translateBooks Whether it should translate written books
      * @return The translated item stack, which may or may not be the same as the given parameter
      */
-    public static ItemStack translateItemStack(ItemStack item, Localized languagePlayer, boolean translateBooks) {
+    @Contract("null, _, _ -> null")
+    public static @Nullable ItemStack translateItemStack(@Nullable ItemStack item, @NotNull Localized languagePlayer, boolean translateBooks) {
         if (item == null || item.getType() == Material.AIR) {
             return item;
         }
+        if (MinecraftVersion.v1_20_5.atOrAbove()) { // 1.20.6+
+            return translateItemStackPost_1_20_6(item, languagePlayer, translateBooks, true);
+        } else {
+            return translateItemStackPre_1_20_6(item, languagePlayer, translateBooks);
+        }
+    }
+
+    private static @NotNull ItemStack translateItemStackPre_1_20_6(@NotNull ItemStack item, @NotNull Localized languagePlayer, boolean translateBooks) {
         NbtCompound compound = null;
         try {
             val nbtTagOptional = NbtFactory.fromItemOptional(item);
@@ -65,8 +75,14 @@ public class ItemStackTranslationUtils {
             // This means the item is just an ItemStack and not a CraftItemStack
             // However we can still translate stuff using the Bukkit ItemMeta API instead of NBT tags
         }
+
+        if (compound == null) {
+            // If the item is not a craft item, use the Bukkit API
+            return translateBukkitItemStack(item, languagePlayer);
+        }
+
         // Translate the contents of shulker boxes
-        if (compound != null && compound.containsKey("BlockEntityTag")) {
+        if (compound.containsKey("BlockEntityTag")) {
             NbtCompound blockEntityTag = compound.getCompoundOrDefault("BlockEntityTag");
             if (blockEntityTag.containsKey("Items")) {
                 NbtBase<?> itemsBase = blockEntityTag.getValue("Items");
@@ -82,46 +98,136 @@ public class ItemStackTranslationUtils {
                 }
             }
         }
-        // If the compound is null, the item is not a CraftItemStack, therefore it doesn't have NBT data
-        if (compound != null) {
-            // try to translate name and lore
-            translateNbtItem(compound, languagePlayer, true);
-            // translate the content of written books
-            if (translateBooks && item.getType() == Material.WRITTEN_BOOK && main().getConf().isBooks()) {
-                if (compound.containsKey("pages")) {
-                    NbtList<String> pages = compound.getList("pages");
-                    Collection<NbtBase<String>> pagesCollection = pages.asCollection();
-                    List<String> newPagesCollection = new ArrayList<>();
-                    for (NbtBase<String> page : pagesCollection) {
-                        try {
-                            BaseComponent[] result = main().getLanguageParser()
-                                    .parseComponent(
-                                            languagePlayer,
-                                            main().getConf().getItemsSyntax(),
-                                            ComponentSerializer.parse(page.getValue())
-                                    );
-                            if (result != null) {
-                                newPagesCollection.add(ComponentSerializer.toString(result));
-                            }
-                        } catch (JsonParseException e) {
-                            String result = translate(
-                                    page.getValue().substring(1, page.getValue().length() - 1),
-                                    languagePlayer,
-                                    main().getConf().getItemsSyntax()
-                            );
-                            if (result != null) {
-                                newPagesCollection.add(
-                                        ComponentSerializer.toString(
-                                                TextComponent.fromLegacyText(result)));
-                            }
+
+        // try to translate name and lore
+        translateNbtItem(compound, languagePlayer, true);
+        // translate the content of written books
+        if (translateBooks && item.getType() == Material.WRITTEN_BOOK && main().getConf().isBooks()) {
+            if (compound.containsKey("pages")) {
+                NbtList<String> pages = compound.getList("pages");
+                Collection<NbtBase<String>> pagesCollection = pages.asCollection();
+                List<String> newPagesCollection = new ArrayList<>();
+                for (NbtBase<String> page : pagesCollection) {
+                    try {
+                        BaseComponent[] result = main().getLanguageParser()
+                                .parseComponent(
+                                        languagePlayer,
+                                        main().getConf().getItemsSyntax(),
+                                        ComponentSerializer.parse(page.getValue())
+                                );
+                        if (result != null) {
+                            newPagesCollection.add(ComponentSerializer.toString(result));
+                        }
+                    } catch (JsonParseException e) {
+                        String result = translate(
+                                page.getValue().substring(1, page.getValue().length() - 1),
+                                languagePlayer,
+                                main().getConf().getItemsSyntax()
+                        );
+                        if (result != null) {
+                            newPagesCollection.add(
+                                    ComponentSerializer.toString(
+                                            TextComponent.fromLegacyText(result)));
                         }
                     }
-                    compound.put("pages", NbtFactory.ofList("pages", newPagesCollection));
                 }
+                compound.put("pages", NbtFactory.ofList("pages", newPagesCollection));
             }
         }
-        // If the item is not a craft item, use the Bukkit API
-        if (compound == null && item.hasItemMeta()) {
+        return item;
+    }
+
+    private static @NotNull ItemStack translateItemStackPost_1_20_6(@NotNull ItemStack item, @NotNull Localized languagePlayer, boolean translateBooks, boolean translateLore) {
+        Optional<WrappedPatchedDataComponentMap> componentMapOpt = WrappedPatchedDataComponentMap.fromItemStack(item);
+        if (!componentMapOpt.isPresent()) {
+            // item stack is not a craft item stack, delegate to the bukkit translator
+            return translateBukkitItemStack(item, languagePlayer);
+        }
+        WrappedPatchedDataComponentMap componentMap = componentMapOpt.get();
+
+        // translate custom name
+        WrappedChatComponent customName = componentMap.getCustomName();
+        if (customName != null) {
+            BaseComponent[] result = main().getLanguageParser()
+                    .parseComponent(
+                            languagePlayer,
+                            main().getConf().getItemsSyntax(),
+                            ComponentSerializer.parse(customName.getJson())
+                    );
+            if (result == null) {
+                componentMap.setCustomName(null);
+            } else {
+                customName.setJson(ComponentSerializer.toString(ComponentUtils.ensureNotItalic(Arrays.stream(result))));
+                componentMap.setCustomName(customName);
+            }
+        }
+
+        if (translateLore) {
+            // translate lore
+            WrappedItemLore itemLore = componentMap.getLore();
+            if (itemLore != null) {
+                List<WrappedChatComponent> newLines = new ArrayList<>();
+                for (WrappedChatComponent line : itemLore.getLines()) {
+                    BaseComponent[] result = main().getLanguageParser()
+                            .parseComponent(
+                                    languagePlayer,
+                                    main().getConf().getItemsSyntax(),
+                                    ComponentSerializer.parse(line.getJson())
+                            );
+                    if (result != null) {
+                        List<List<BaseComponent>> splitLoreLines = ComponentUtils.splitByNewLine(Arrays.asList(result));
+                        newLines.addAll(splitLoreLines.stream()
+                                .map(comps -> ComponentUtils.ensureNotItalic(comps.stream()))
+                                .map(ComponentSerializer::toString)
+                                .map(WrappedChatComponent::fromJson)
+                                .collect(Collectors.toList()));
+                    }
+                }
+                itemLore.setLines(newLines);
+                componentMap.setLore(itemLore);
+            }
+
+            // translate tooltips of shulker boxes (and other containers)
+            WrappedItemContainerContents containerContents = componentMap.getContainer();
+            if (containerContents != null) {
+                List<ItemStack> newItems = containerContents.getItems()
+                        .stream()
+                        .map(containerItem -> translateItemStackPost_1_20_6(containerItem.clone(), languagePlayer, false, false))
+                        .collect(Collectors.toList());
+                containerContents.setItems(newItems);
+                componentMap.setContainer(containerContents);
+            }
+        }
+
+        // translate contents of written books
+        if (translateBooks && main().getConfig().isBooks()) {
+            WrappedWrittenBookContent bookContent = componentMap.getWrittenBookContent();
+            if (bookContent != null) {
+                List<WrappedFilterable<WrappedChatComponent>> newPages = new ArrayList<>();
+                for (WrappedFilterable<WrappedChatComponent> page : bookContent.getPages()) {
+                    WrappedChatComponent raw = page.getRaw();
+                    BaseComponent[] result = main().getLanguageParser()
+                            .parseComponent(
+                                    languagePlayer,
+                                    main().getConf().getItemsSyntax(),
+                                    ComponentSerializer.parse(raw.getJson())
+                            );
+                    if (result != null) {
+                        raw.setJson(ComponentSerializer.toString(result));
+                        page.setRaw(raw);
+                        newPages.add(page);
+                    }
+                }
+                bookContent.setPages(newPages);
+                componentMap.setWrittenBookContent(bookContent);
+            }
+        }
+
+        return item;
+    }
+
+    private static @NotNull ItemStack translateBukkitItemStack(@NotNull ItemStack item, @NotNull Localized languagePlayer) {
+        if (item.hasItemMeta()) {
             ItemMeta meta = item.getItemMeta();
             if (meta.hasDisplayName()) {
                 meta.setDisplayName(translate(meta.getDisplayName(),
@@ -149,7 +255,7 @@ public class ItemStackTranslationUtils {
      * @param languagePlayer The language player to translate for
      * @param translateLore  Whether to attempt to translate the lore of the item
      */
-    public static void translateNbtItem(NbtCompound compound, Localized languagePlayer, boolean translateLore) {
+    public static void translateNbtItem(@NotNull NbtCompound compound, @NotNull Localized languagePlayer, boolean translateLore) {
         if (!compound.containsKey("display")) {
             return;
         }

siberanka/TwiTriton

Port of bf02b10d7e971400d099becc642c614c4460ee6e to v4

triton-spigot/src/main/java/com/rexcantor64/triton/spigot/utils/ItemStackTranslationUtils.java
@@ -4,6 +4,8 @@ import com.comphenix.protocol.reflect.FuzzyReflection;
 import com.comphenix.protocol.reflect.accessors.Accessors;
 import com.comphenix.protocol.reflect.accessors.MethodAccessor;
 import com.comphenix.protocol.utility.MinecraftReflection;
+import com.comphenix.protocol.utility.MinecraftVersion;
+import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import com.comphenix.protocol.wrappers.nbt.NbtBase;
 import com.comphenix.protocol.wrappers.nbt.NbtCompound;
 import com.comphenix.protocol.wrappers.nbt.NbtFactory;
@@ -11,16 +13,25 @@ import com.comphenix.protocol.wrappers.nbt.NbtList;
 import com.google.gson.JsonSyntaxException;
 import com.rexcantor64.triton.api.language.Localized;
 import com.rexcantor64.triton.spigot.SpigotTriton;
+import com.rexcantor64.triton.spigot.wrappers.items.WrappedFilterable;
+import com.rexcantor64.triton.spigot.wrappers.items.WrappedItemContainerContents;
+import com.rexcantor64.triton.spigot.wrappers.items.WrappedItemLore;
+import com.rexcantor64.triton.spigot.wrappers.items.WrappedPatchedDataComponentMap;
+import com.rexcantor64.triton.spigot.wrappers.items.WrappedWrittenBookContent;
 import com.rexcantor64.triton.utils.ComponentUtils;
 import lombok.val;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class ItemStackTranslationUtils {
@@ -28,7 +39,7 @@ public class ItemStackTranslationUtils {
     private static final MethodAccessor NBT_DESERIALIZER_METHOD;
 
     static {
-        val mojangsonParserClass = MinecraftReflection.getMinecraftClass("nbt.MojangsonParser", "MojangsonParser");
+        val mojangsonParserClass = MinecraftReflection.getMinecraftClass("nbt.TagParser", "nbt.MojangsonParser", "MojangsonParser");
         FuzzyReflection fuzzy = FuzzyReflection.fromClass(mojangsonParserClass);
         val method = fuzzy.getMethodByReturnTypeAndParameters("deserializeNbtCompound", MinecraftReflection.getNBTCompoundClass(), String.class);
         NBT_DESERIALIZER_METHOD = Accessors.getMethodAccessor(method);
@@ -47,10 +58,19 @@ public class ItemStackTranslationUtils {
      * @param translateBooks Whether it should translate written books
      * @return The translated item stack, which may or may not be the same as the given parameter
      */
-    public static ItemStack translateItemStack(ItemStack item, Localized languagePlayer, boolean translateBooks) {
+    @Contract("null, _, _ -> null")
+    public static @Nullable ItemStack translateItemStack(@Nullable ItemStack item, @NotNull Localized languagePlayer, boolean translateBooks) {
         if (item == null || item.getType() == Material.AIR) {
             return item;
         }
+        if (MinecraftVersion.v1_20_5.atOrAbove()) { // 1.20.6+
+            return translateItemStackPost_1_20_6(item, languagePlayer, translateBooks, true);
+        } else {
+            return translateItemStackPre_1_20_6(item, languagePlayer, translateBooks);
+        }
+    }
+
+    private static @NotNull ItemStack translateItemStackPre_1_20_6(@NotNull ItemStack item, @NotNull Localized languagePlayer, boolean translateBooks) {
         NbtCompound compound = null;
         try {
             val nbtTagOptional = NbtFactory.fromItemOptional(item);
@@ -60,8 +80,14 @@ public class ItemStackTranslationUtils {
             // This means the item is just an ItemStack and not a CraftItemStack
             // However we can still translate stuff using the Bukkit ItemMeta API instead of NBT tags
         }
+
+        if (compound == null) {
+            // If the item is not a craft item, use the Bukkit API
+            return translateBukkitItemStack(item, languagePlayer);
+        }
+
         // Translate the contents of shulker boxes
-        if (compound != null && compound.containsKey("BlockEntityTag")) {
+        if (compound.containsKey("BlockEntityTag")) {
             NbtCompound blockEntityTag = compound.getCompoundOrDefault("BlockEntityTag");
             if (blockEntityTag.containsKey("Items")) {
                 NbtBase<?> itemsBase = blockEntityTag.getValue("Items");
@@ -77,45 +103,133 @@ public class ItemStackTranslationUtils {
                 }
             }
         }
-        // If the compound is null, the item is not a CraftItemStack, therefore it doesn't have NBT data
-        if (compound != null) {
-            // try to translate name and lore
-            translateNbtItem(compound, languagePlayer, true);
-            // translate the content of written books
-            if (translateBooks && item.getType() == Material.WRITTEN_BOOK && main().getConfig().isBooks()) {
-                if (compound.containsKey("pages")) {
-                    NbtList<String> pages = compound.getList("pages");
-                    Collection<NbtBase<String>> pagesCollection = pages.asCollection();
-                    List<String> newPagesCollection = new ArrayList<>();
-                    for (NbtBase<String> page : pagesCollection) {
-                        try {
-                            main().getMessageParser()
-                                    .translateComponent(
-                                            ComponentUtils.deserializeFromJson(page.getValue()),
-                                            languagePlayer,
-                                            main().getConfig().getItemsSyntax()
-                                    )
-                                    .map(ComponentUtils::serializeToJson)
-                                    .ifChanged(newPagesCollection::add)
-                                    .ifUnchanged(() -> newPagesCollection.add(page.getValue()));
-                        } catch (JsonSyntaxException e) {
-                            val pageString = page.getValue();
-                            main().getMessageParser()
-                                    .translateString(
-                                            pageString,
-                                            languagePlayer,
-                                            main().getConfig().getItemsSyntax()
-                                    )
-                                    .ifChanged(newPagesCollection::add)
-                                    .ifUnchanged(() -> newPagesCollection.add(pageString));
-                        }
+
+        // try to translate name and lore
+        translateNbtItem(compound, languagePlayer, true);
+        // translate the content of written books
+        if (translateBooks && item.getType() == Material.WRITTEN_BOOK && main().getConfig().isBooks()) {
+            if (compound.containsKey("pages")) {
+                NbtList<String> pages = compound.getList("pages");
+                Collection<NbtBase<String>> pagesCollection = pages.asCollection();
+                List<String> newPagesCollection = new ArrayList<>();
+                for (NbtBase<String> page : pagesCollection) {
+                    try {
+                        main().getMessageParser()
+                                .translateComponent(
+                                        ComponentUtils.deserializeFromJson(page.getValue()),
+                                        languagePlayer,
+                                        main().getConfig().getItemsSyntax()
+                                )
+                                .map(ComponentUtils::serializeToJson)
+                                .ifChanged(newPagesCollection::add)
+                                .ifUnchanged(() -> newPagesCollection.add(page.getValue()));
+                    } catch (JsonSyntaxException e) {
+                        val pageString = page.getValue();
+                        main().getMessageParser()
+                                .translateString(
+                                        pageString,
+                                        languagePlayer,
+                                        main().getConfig().getItemsSyntax()
+                                )
+                                .ifChanged(newPagesCollection::add)
+                                .ifUnchanged(() -> newPagesCollection.add(pageString));
                     }
-                    compound.put("pages", NbtFactory.ofList("pages", newPagesCollection));
                 }
+                compound.put("pages", NbtFactory.ofList("pages", newPagesCollection));
             }
         }
-        // If the item is not a craft item, use the Bukkit API
-        if (compound == null && item.hasItemMeta()) {
+        return item;
+    }
+
+    private static @NotNull ItemStack translateItemStackPost_1_20_6(@NotNull ItemStack item, @NotNull Localized languagePlayer, boolean translateBooks, boolean translateLore) {
+        Optional<WrappedPatchedDataComponentMap> componentMapOpt = WrappedPatchedDataComponentMap.fromItemStack(item);
+        if (!componentMapOpt.isPresent()) {
+            // item stack is not a craft item stack, delegate to the bukkit translator
+            return translateBukkitItemStack(item, languagePlayer);
+        }
+        WrappedPatchedDataComponentMap componentMap = componentMapOpt.get();
+
+        // translate custom name
+        WrappedChatComponent customName = componentMap.getCustomName();
+        if (customName != null) {
+            main().getMessageParser()
+                    .translateComponent(
+                            WrappedComponentUtils.deserialize(customName),
+                            languagePlayer,
+                            main().getConfig().getItemsSyntax()
+                    )
+                    .map(ComponentUtils::ensureNotItalic)
+                    .map(WrappedComponentUtils::serialize)
+                    .getResultOrToRemove(() -> null)
+                    .ifPresent(componentMap::setCustomName);
+        }
+
+        if (translateLore) {
+            // translate lore
+            WrappedItemLore itemLore = componentMap.getLore();
+            if (itemLore != null) {
+                List<WrappedChatComponent> newLines = new ArrayList<>();
+                for (WrappedChatComponent line : itemLore.getLines()) {
+                    main().getMessageParser()
+                            .translateComponent(
+                                    WrappedComponentUtils.deserialize(line),
+                                    languagePlayer,
+                                    main().getConfig().getItemsSyntax()
+                            )
+                            .map(ComponentUtils::splitByNewLine)
+                            .ifChanged(result -> newLines.addAll(
+                                    result.stream()
+                                            .map(ComponentUtils::ensureNotItalic)
+                                            .map(WrappedComponentUtils::serialize)
+                                            .collect(Collectors.toList())
+                            ))
+                            .ifUnchanged(() -> newLines.add(line));
+                }
+                itemLore.setLines(newLines);
+                componentMap.setLore(itemLore);
+            }
+
+            // translate tooltips of shulker boxes (and other containers)
+            WrappedItemContainerContents containerContents = componentMap.getContainer();
+            if (containerContents != null) {
+                List<ItemStack> newItems = containerContents.getItems()
+                        .stream()
+                        .map(containerItem -> translateItemStackPost_1_20_6(containerItem.clone(), languagePlayer, false, false))
+                        .collect(Collectors.toList());
+                containerContents.setItems(newItems);
+                componentMap.setContainer(containerContents);
+            }
+        }
+
+        // translate contents of written books
+        if (translateBooks && main().getConfig().isBooks()) {
+            WrappedWrittenBookContent bookContent = componentMap.getWrittenBookContent();
+            if (bookContent != null) {
+                List<WrappedFilterable<WrappedChatComponent>> newPages = new ArrayList<>();
+                for (WrappedFilterable<WrappedChatComponent> page : bookContent.getPages()) {
+                    main().getMessageParser()
+                            .translateComponent(
+                                    WrappedComponentUtils.deserialize(page.getRaw()),
+                                    languagePlayer,
+                                    main().getConfig().getItemsSyntax()
+                            )
+                            .map(WrappedComponentUtils::serialize)
+                            .ifChanged(pageContents -> {
+                                page.setRaw(pageContents);
+                                newPages.add(page);
+                            })
+                            .ifUnchanged(() -> newPages.add(page));
+                }
+                bookContent.setPages(newPages);
+                componentMap.setWrittenBookContent(bookContent);
+            }
+        }
+
+        return item;
+    }
+
+    private static @NotNull ItemStack translateBukkitItemStack(@NotNull ItemStack item, @NotNull Localized languagePlayer) {
+        if (item.hasItemMeta()) {
             ItemMeta meta = item.getItemMeta();
             if (meta.hasDisplayName()) {
                 main().getMessageParser()
@@ -149,7 +263,7 @@ public class ItemStackTranslationUtils {
      * @param languagePlayer The language player to translate for
      * @param translateLore  Whether to attempt to translate the lore of the item
      */
-    public static void translateNbtItem(NbtCompound compound, Localized languagePlayer, boolean translateLore) {
+    public static void translateNbtItem(@NotNull NbtCompound compound, @NotNull Localized languagePlayer, boolean translateLore) {
         if (!compound.containsKey("display")) {
             return;
         }